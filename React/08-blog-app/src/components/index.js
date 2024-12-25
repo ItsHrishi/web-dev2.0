f6b3fee54414f7f85d07d2010b54d9e3622bcd8f
Hrishikesh <ghrishikesh77@gmail.com>
2024-12-25 13:46:27 +0530

    blog app learning complete

---
+++ React/08-blog-app/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import PostForm from "./post-form/PostForm";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Input from "./Input";
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+
+export {
+  Container,
+  Footer,
+  Header,
+  PostForm,
+  AuthLayout,
+  Button,
+  Input,
+  Login,
+  Logo,
+  PostCard,
+  RTE,
+  Select,
+  Signup,
+  LogoutBtn,
+};
